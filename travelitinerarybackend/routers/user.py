fb4abb4855635a95b34a2b3fa95551b814f543dc
khaled soliman <khaled.soliman97@gmail.com>
2025-07-16 03:59:02 +0300

    adds auth feature
    - adds SECRET_KEY and ALGORITHM that will be used by jwt
    - in main adds user router
    - adds security module which create_access_token, get_password_hash, verify_password, get_user, authenticate_user, and get_current_user TODO make sure that every user can access his itineraries onlyy
    - adds user model
    - adds user handlers which are /register and /token
    - adds test module for security

---
+++ travelitinerarybackend/routers/user.py
@@ -0,0 +1,37 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from typing_extensions import Annotated
+
+from travelitinerarybackend.database import database, user_table
+from travelitinerarybackend.models.user import UserIn
+from travelitinerarybackend.security import (
+    authenticate_user,
+    create_access_token,
+    get_password_hash,
+    get_user,
+)
+
+router = APIRouter()
+
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def register_user(user: UserIn):
+    if await get_user(user.email):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
+        )
+    hashed_password = get_password_hash(user.password)
+
+    query = user_table.insert().values(email=user.email, password=hashed_password)
+    print(query)
+
+    await database.execute(query)
+
+    return {"detail": "User registered successfully"}
+
+
+@router.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await authenticate_user(form_data.username, form_data.password)
+    access_token = create_access_token(user.email)
+    return {"access_token": access_token, "token_type": "bearer"}
